be54edb5cfa502ebdeeec6673885f428fe2617ea
Banky <kingmorty@protonmail.com>
2023-05-27 17:18:31 +0100

    Add logic to check winning conditions

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ function GameController() {
   const playerOne = Player("Player 1", 1);
   const playerTwo = Player("Player 2", 2);
 
+  let turns = 0;
   let currentPlayer = playerOne;
 
   const getCurrentPlayer = () => {
     return currentPlayer;
   };
-  // console.log(`Next player: ${getCurrentPlayer().playerName}`);
 
   const switchPlayer = () => {
     if (currentPlayer === playerOne) {
@@ -56,13 +56,69 @@ function GameController() {
     }
   };
 
-  const playRound = (row, column) => {
-    if (gameboard.markCell(getCurrentPlayer().playerID, row, column)) {
-      switchPlayer();
+  const win = (turns, id) => {
+    const board = gameboard.getBoard();
+
+    if (turns > 4) {
+      if (board[0][0] === id && id > 0) {
+        if (board[0][1] === id && board[0][2] == id) {
+          return true;
+        } else if (board[1][0] === id && board[2][0] == id) {
+          return true;
+        } else if (board[1][1] === id && board[2][2] == id) {
+          return true;
+        }
+      } else if (board[0][2] === id && id > 0) {
+        if (board[1][1] === id && board[2][0] == id) {
+          return true;
+        } else if (board[1][2] === id && board[2][2] == id) {
+          return true;
+        }
+      } else if (board[0][1] === id && id > 0) {
+        if (board[1][1] === id && board[2][1] == id) {
+          return true;
+        }
+      } else if (board[1][0] === id && id > 0) {
+        if (board[1][1] === id && board[1][2] == id) {
+          return true;
+        }
+      } else if (board[2][0] === id && id > 0) {
+        if (board[2][1] === id && board[2][2] == id) {
+          return true;
+        }
+      }
+    } else {
+      return false;
     }
+  };
+
+  const playRound = (row, column) => {
+    console.log(`Current player: ${getCurrentPlayer().playerName}`);
+
+    // If a legal move is played, insert the player id in the board
+    if (gameboard.markCell(getCurrentPlayer().playerID, row, column)) {
+      // increment the turn counter
+      turns++;
+
+      // if any of the win conditions are satisfied, end the game
+      if (win(turns, getCurrentPlayer().playerID)) {
+        console.log(`${getCurrentPlayer().playerName} wins!!`);
+        return;
+      }
+
+      // if there's no winner yet, switch players
+      switchPlayer();
+
+      // if the move is illegal or game has ended, provide an error msg
+    } else {
+      console.log("Invalid move or game over");
+    }
+
     console.log(gameboard.getBoard());
-    // console.log(`Next player: ${getCurrentPlayer().playerName}`);
+    console.log(`Next player: ${getCurrentPlayer().playerName}`);
   };
 
   return { playRound };
 }
+
+const newGame = GameController();
