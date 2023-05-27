dd0bc1a77c14494573aeb93a1afdc67a1eed39a4
Banky <kingmorty@protonmail.com>
2023-05-27 14:28:44 +0100

    Add basic logic to let players pick cells

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function GameGrid() {
+function Gameboard() {
   const board = [];
   const rows = 3;
   const columns = 3;
 
-  // creates the 2d array matching to the gameboard's grid
+  // creates the 2d array matching to the gameboard's grids
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
@@ -11,18 +11,58 @@ function GameGrid() {
     }
   }
 
-  // gets the game grid
-  const getGrid = () => {
+  // gets the game board
+  const getBoard = () => {
     return board;
   };
 
-  // allows a player to place a marker on a grid cell
-  const markCell = (player, row, column) => {
+  // allows a player to place a marker in a board cell
+  const markCell = (playerID, row, column) => {
     const currentGrid = board;
     if (currentGrid[row][column] === 0) {
-      currentGrid[row][column] = player;
+      currentGrid[row][column] = playerID;
+      return true;
+    }
+    return false;
+  };
+
+  return { getBoard, markCell };
+}
+
+function Player(name, id) {
+  const playerName = name;
+  const playerID = id;
+
+  return { playerName, playerID };
+}
+
+function GameController() {
+  const gameboard = Gameboard();
+  const playerOne = Player("Player 1", 1);
+  const playerTwo = Player("Player 2", 2);
+
+  let currentPlayer = playerOne;
+
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  };
+  // console.log(`Next player: ${getCurrentPlayer().playerName}`);
+
+  const switchPlayer = () => {
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+    } else {
+      currentPlayer = playerOne;
     }
   };
 
-  return { getGrid, markCell };
+  const playRound = (row, column) => {
+    if (gameboard.markCell(getCurrentPlayer().playerID, row, column)) {
+      switchPlayer();
+    }
+    console.log(gameboard.getBoard());
+    // console.log(`Next player: ${getCurrentPlayer().playerName}`);
+  };
+
+  return { playRound };
 }
